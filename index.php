820d96f213583973fb5662f3d91b9b0f1584b0bc
royyy-han <mroyhan1808@gmail.com>
2025-01-06 12:10:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>
+    <?php
+    echo "Hello World!!! Ini adalah halaman php";
+    ?>
+    </H1>
+    
+</body>
+</html>
